fd180fd55feb687b7c1ec1f6bca0967464d8ac16
unknown <Thang24122k@gmail.com>
2021-05-10 22:09:07 +0700

    finished all

--- 1/js/index.js
+++ 1/js/index.js
@@ -4,26 +4,26 @@ function onClick(id) {
     var elementIconSmall = document.getElementById('icon_small');
 
     if (id == 'angry') {
-        elementAngry.classList.add('d-block');
+        elementAngry.classList.add('d-block', 'show');
         elementSmile.classList.add('d-none');
         elementIconSmall.classList.add('d-none');
 
-        elementSmile.classList.remove('d-block');
-        elementIconSmall.classList.remove('d-block');
+        elementSmile.classList.remove('d-block', 'hide');
+        elementIconSmall.classList.remove('d-block', 'hide');
     } else if (id == 'smile') {
         elementAngry.classList.add('d-none');
-        elementSmile.classList.add('d-block');
+        elementSmile.classList.add('d-block','show');
         elementIconSmall.classList.add('d-none');
 
-        elementAngry.classList.remove('d-block');
-        elementIconSmall.classList.remove('d-block');
+        elementAngry.classList.remove('d-block', 'hide');
+        elementIconSmall.classList.remove('d-block', 'hide');
     } else if (id == 'icon_small') {
         elementAngry.classList.add('d-none');
         elementSmile.classList.add('d-none');
-        elementIconSmall.classList.add('d-block');
+        elementIconSmall.classList.add('d-block', 'show');
 
-        elementAngry.classList.remove('d-block');
-        elementSmile.classList.remove('d-block');
+        elementAngry.classList.remove('d-block', 'hide');
+        elementSmile.classList.remove('d-block', 'hide');
     }
 }
 
