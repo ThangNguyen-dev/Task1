6254eb4863efd5ce2c99a30aca95e5be43c33df7
unknown <Thang24122k@gmail.com>
2021-05-26 18:29:44 +0700

    finished all

--- 1/js/index.js
+++ 1/js/index.js
@@ -7,31 +7,31 @@ var sadIcon = document.getElementById('sadIcon');
 function onClick(id) {
 
     if (id == 'smile') {
-        smileIcon.classList.add('d-block');
+        smileIcon.classList.add('d-block', 'show');
         smileIcon.classList.remove('d-none');
 
         angryIcon.classList.remove('d-block');
-        angryIcon.classList.add('d-none');
+        angryIcon.classList.add('d-none', 'hidden');
 
         sadIcon.classList.remove('d-block');
-        sadIcon.classList.add('d-none');
+        sadIcon.classList.add('d-none', 'hidden');
     } else if (id == 'angry') {
-        angryIcon.classList.add('d-block');
-        angryIcon.classList.add('d-block');
+        angryIcon.classList.add('d-block', 'show');
+        angryIcon.classList.remove('d-none');
 
         smileIcon.classList.remove('d-block');
-        smileIcon.classList.add('d-none');
+        smileIcon.classList.add('d-none', 'hidden');
 
-        sadIcon.classList.add('d-none');
+        sadIcon.classList.add('d-none', 'hidden');
         sadIcon.classList.remove('d-block');
     } else if (id == 'sad') {
-        sadIcon.classList.add('d-block');
+        sadIcon.classList.add('d-block', 'show');
         sadIcon.classList.remove('d-none');
 
-        smileIcon.classList.add('d-none');
+        smileIcon.classList.add('d-none', 'hidden');
         smileIcon.classList.remove('d-block');
 
-        angryIcon.classList.add('d-none');
+        angryIcon.classList.add('d-none', 'hidden');
         angryIcon.classList.remove('d-block');
     }
 }
